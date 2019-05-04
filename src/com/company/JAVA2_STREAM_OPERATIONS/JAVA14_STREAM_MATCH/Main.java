b8f11772bbd1256be2f52bf701248ec98bf19db2
Manoj Bagari <manojbagari@gmail.com>
2019-05-05 00:12:00 +0530

    JAVA STREAM MATCH EXAMPLE

---
+++ src/com/company/JAVA2_STREAM_OPERATIONS/JAVA14_STREAM_MATCH/Main.java
@@ -0,0 +1,46 @@
+package com.company.JAVA2_STREAM_OPERATIONS.JAVA14_STREAM_MATCH;
+
+import java.util.stream.DoubleStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/*
+        The Streams API supports different types of match operations on stream elements.
+
+        The following methods in the Stream interface are used to perform match operations:
+
+        boolean allMatch(Predicate<? super T> predicate)
+        boolean anyMatch(Predicate<? super T> predicate)
+        boolean noneMatch(Predicate<? super  T> predicate)
+
+        The primitive type streams such as
+        IntStream, LongStream, and DoubleStream
+        also contain the same methods that work
+        with a predicate and an optional one for primitive types.
+
+        All match operations are terminal operations.
+        They are also short-circuiting operations.
+        A short-circuiting operation may not have
+        to process the entire stream to return the result.
+
+        The following code shows how to perform match operations on streams.
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
